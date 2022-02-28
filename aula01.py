265fb87da5b241fcbe842fa4379241038bb53a7e
Ygor Alexsandro <91572599+AlexYgor24@users.noreply.github.com>
2022-02-28 14:32:00 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,21 @@
+nome = input ('Qual seu nome ? ' )
+print('Um grande prazer em conhecer você : ',nome )
+dia = input('Qual o dia do seu nascimento ? ' )
+mes = input('Qual o mês : ')
+ano = input('Qual o ano :')
+print (nome, 'confere comigo o dia do seu nascimento é :', dia ,'/',mes,'/',ano)
+contato = int(input('Qual seria seu número de contato :'))
+print(nome,'seu número está correto : ')
+cidade = (input('Qual é seu estado :'))
+print(nome,'Agradeço pelas informações , vamos dar a última olhada :')
+print('suas informações são :',nome,dia,mes,ano,contato,cidade)
+
+
+
+
+
+
+
+
+
+
